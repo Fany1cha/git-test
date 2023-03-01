3adfff15c324e89fd3f364c506c203b6257de9b4
root <1471081165@qq.com>
2023-03-01 21:25:15 +0800

    third commit

--- src/main/java/com/Fanyicha.java
+++ src/main/java/com/Fanyicha.java
@@ -10,5 +10,6 @@ public class Fanyicha {
         System.out.println("hello github");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git3");
     }
 }
