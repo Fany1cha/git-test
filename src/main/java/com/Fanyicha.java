a34bdc5e22edaf0b66061940f4915e387fbc492b
root <1471081165@qq.com>
2023-03-01 21:31:39 +0800

    hot-fix first commit

--- src/main/java/com/Fanyicha.java
+++ src/main/java/com/Fanyicha.java
@@ -11,5 +11,6 @@ public class Fanyicha {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
