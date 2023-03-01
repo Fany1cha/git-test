e3d4024f1c98f85e41759e251539c0c343ede93b
root <1471081165@qq.com>
2023-03-01 21:23:15 +0800

    first commit

---
+++ src/main/java/com/Fanyicha.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author 张孟齐
+ * @version 1.0
+ **/
+public class Fanyicha {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
